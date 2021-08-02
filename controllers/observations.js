123b06cffd250e4b0038516541451f0d833b541a
Jorge Paul <jserranopaul@gmail.com>
2021-08-02 15:41:50 +0200

    Fixed query for report

--- controllers/observations.js
+++ controllers/observations.js
@@ -2,17 +2,65 @@ import Observation from '../models/Observation.js';
 import downloadReport from '../utils/downloadReport.js';
 
 export const getReport = async (req, res) => {
+  const { startDate, endDate } = req.params;
+  const query = {
+    date_time_original: { $gte: startDate, $lt: endDate }
+  };
   const fields = [
     {
       label: 'Observation ID',
       value: '_id'
     },
     {
+      label: 'Deployment ID',
+      value: 'deployment_id'
+    },
+    {
+      label: 'Sequence ID',
+      value: 'sequence_id'
+    },
+    {
+      label: 'Cat ID',
+      value: 'specimen'
+    },
+    {
+      label: 'Pattern',
+      value: 'pattern'
+    },
+    {
+      label: 'Bicolor',
+      value: 'bicolor'
+    },
+    {
+      label: 'Long hair',
+      value: 'longHair'
+    },
+    {
+      label: 'Sex',
+      value: 'sex'
+    },
+    {
+      label: 'Notched ear',
+      value: 'notched'
+    },
+    {
+      label: 'Collar',
+      value: 'collar'
+    },
+    {
+      label: 'Latitude',
+      value: 'location.coordinates[1]'
+    },
+    {
+      label: 'Longitude',
+      value: 'location.coordinates[0]'
+    },
+    {
       label: 'Date',
       value: 'date_time_original'
     }
   ];
-  const data = await Observation.find();
+  const data = await Observation.find(query);
   return downloadReport(res, 'report.csv', fields, data);
 };
 
